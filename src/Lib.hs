08d566f3a28370d2f63aa81ef27b405d59f6ddc0
Teddy Wing <git@teddywing.com>
2017-07-30 02:22:23 +0200

    Experiment with 'regex-tdfa'
    * Add the 'regex-tdfa' library for RegEx handling
    * Experiment with matching a Git SHA

--- src/Lib.hs
+++ src/Lib.hs
@@ -2,5 +2,13 @@ module Lib
     ( someFunc
     ) where
 
+import Text.Regex.TDFA
+
 someFunc :: IO ()
-someFunc = putStrLn "someFunc"
+someFunc
+    | rex == True = putStrLn "Match!!"
+    | otherwise   = putStrLn "No match"
+
+
+rex :: Bool
+rex = "75ac7b18a009ffe7a77a17a61d95c01395f36b44" =~ "^[0-9a-f]{5,40}$"
