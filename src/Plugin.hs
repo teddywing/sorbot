a96f5a87746536a045a0a9fd07f65072d680589a
Teddy Wing <git@teddywing.com>
2017-07-30 19:48:31 +0200

    Plugin.hs: Add some minimal documentation
    Write some short documentation above the function definitions to remind
    myself later about what they're supposed to do.

--- src/Plugin.hs
+++ src/Plugin.hs
@@ -9,18 +9,22 @@ import Text.Regex.TDFA
 import Plugin.Base
 import Plugin.GitHubCommit
 
+-- | Get the first plugin that matches the given message text.
 matchPlugin :: String -> Maybe Plugin
 matchPlugin message = firstPlugin $ matchPlugins message plugins
   where
     firstPlugin []     = Nothing
     firstPlugin (p:ps) = Just p
 
+-- | Filter the list of plugins to those that match the given message.
 matchPlugins :: String -> [Plugin] -> [Plugin]
 matchPlugins message plugins = [p | p <- plugins, message =~ matchRegex p]
 
+-- | Run the action belonging to the plugin, stored in its `perform` field.
 performPlugin :: Plugin -> String -> String
 performPlugin p message = perform p $ message =~ matchRegex p
 
+-- | The list of plugins to load
 plugins :: [Plugin]
 plugins =
     [ gitHubCommit
