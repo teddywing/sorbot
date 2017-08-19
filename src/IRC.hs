54f23f1d77a3623d6e18c0a55b622d4cfab60406
Teddy Wing <git@teddywing.com>
2017-08-19 13:27:09 +0200

    IRC.hs: Reformat record update syntax
    Previously I had moved the `{` to the previous line because I was
    getting compilation errors with it on a new line. Turns out whitespace
    is important here. We just need to indent it an extra time and the
    compiler understands us. Now this section matches the style of the rest
    of the project.

--- src/IRC.hs
+++ src/IRC.hs
@@ -17,10 +17,11 @@ connectIRC :: B.ByteString -> Int -> T.Text -> IO ()
 connectIRC host port nick = do
     conn <- IRC.connectWithTLS host port 1
     let cfg = IRC.defaultIRCConf nick
-    let cfg' = cfg {
-          IRC._eventHandlers = handlePrivmsg : IRC._eventHandlers cfg
-        , IRC._channels = ["#test-chan-13513"]
-        }
+    let cfg' = cfg
+            {
+              IRC._eventHandlers = handlePrivmsg : IRC._eventHandlers cfg
+            , IRC._channels = ["#test-chan-13513"]
+            }
     IRC.start conn cfg'
 
 handlePrivmsg :: IRC.EventHandler s
