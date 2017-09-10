613e0d9a67d29b2bb27efd7ca993d4108724b30a
Teddy Wing <git@teddywing.com>
2017-09-11 00:39:40 +0200

    GitHubCommit: Try to add `Bot` monad
    This is an attempt at wrapping the functions in this plugin with the new
    `Bot` monad. It didn't go very well.
    
    These changes are the result of some initial code and then a lot of
    fiddling to try to resolve compiler errors, many of which are still
    present.
    
    The ultimate goal is to be able to wrap the functions in `Bot` so that
    we can use the language configuration option to determine which
    translation to use.
    
    Thanks to 'dmwit' on Freenode#haskell for looking at my code and giving
    me suggestions for my type errors.

--- src/I18n.hs
+++ src/I18n.hs
@@ -12,16 +12,20 @@ import qualified Data.Text as T
 data Locale = EN | FR deriving (Show)
 
 data Message
-    = GitHubCommitRepoURLNotFound
+    = GitHubCommitDescription
+    | GitHubCommitRepoURLNotFound
     | GitRemoteSetOriginUpdatedRepoURL T.Text
 
 translate_en_US :: Message -> T.Text
+translate_en_US GitHubCommitDescription = "Generate a commit URL based on the given SHA."
 translate_en_US GitHubCommitRepoURLNotFound = "I couldn't find a repo URL for \
     \this channel. Try `git remote set origin REPO_URL`."
 translate_en_US (GitRemoteSetOriginUpdatedRepoURL url) =
     "I updated the channel's repo URL to '" `T.append` url `T.append` "'."
 
 translate_fr_FR :: Message -> T.Text
+-- TODO: translate
+translate_fr_FR GitHubCommitDescription = "Generate a commit URL based on the given SHA."
 translate_fr_FR GitHubCommitRepoURLNotFound = "Je n'ai pas trouvé une URL de \
     \repo pour ce channel. Essaye `git remote set origin REPO_URL`."
 translate_fr_FR (GitRemoteSetOriginUpdatedRepoURL url) =
