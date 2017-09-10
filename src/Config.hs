cd366dc004b82f0ea937da231cbc1c9abfdca934
Teddy Wing <git@teddywing.com>
2017-09-11 00:57:50 +0200

    Add Config
    An old config object, from before I decided to go with the `Bot` type.
    Committed here just to show some of the process. We'll be removing it
    soon.

---
+++ src/Config.hs
@@ -0,0 +1,16 @@
+-- module Config
+--     ( Config(..)
+--     ) where
+--
+-- import Control.Monad.Reader (ReaderT)
+--
+-- import CliOptions (Options)
+--
+-- -- type Config = Options
+--
+-- -- data Config a = ReaderT Options a
+-- -- newtype Config a = ReaderT Options
+-- --     { config :: Options
+--
+-- newtype Config = Config
+--     { 
